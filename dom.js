12ec605d0307a519f7fa559e5d54b17fd258ab44
Eliana <elianarlivingston@gmail.com>
2021-05-17 12:15:55 -0300

    fix/ arregla color de ganancia

--- dom.js
+++ dom.js
@@ -276,8 +276,8 @@ const renderOperaciones = (array, element) => {
       <div class="col s3">${el.categoria}</div>
       <div class="col s2">${el.fecha}</div>
       <div class="col s2 ${
-        el.tipo === "Ganancia" ? "montoGreen" : "montoRed"
-      }"> ${el.tipo === "Ganancia" ? "+ $" + el.monto : "- $" + el.monto}</div>
+        el.tipo === "spending" ? "montoGreen" : "montoRed"
+      }"> ${el.tipo === "spending" ? "+ $" + el.monto : "- $" + el.monto}</div>
       <div class="col s2"> 
         <div class="margin-right-plus blue-text operation-edit manito" id="${
           el.id
